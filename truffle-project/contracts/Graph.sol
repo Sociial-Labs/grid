a8f82651f4f382049c2fbb424bb4dcfd2ee49d32
Nathanael Mueller <nathanaelmueller@Nates-Mac.local>
2020-07-20 21:11:54 -0700

    smart contract updated to read better and update mappings appropriately

--- truffle-project/contracts/Graph.sol
+++ truffle-project/contracts/Graph.sol
@@ -2,26 +2,32 @@
 pragma solidity >=0.4.25 <0.7.0;
 
 contract Graph {
-    event FollowerAdded(address parent, address child);
-    event FollowerDeleted(address child, address parent);
-    event Following(address parent, address child);
+    event Followed(address child, address parent);
+    event Unfollowed(address child, address parent);
+    event FollowerDeleted(address parent, address child);
 
     mapping(address => mapping(address => bool)) followedBy;
     mapping(address => mapping(address => bool)) following;
     mapping(address => uint32) followerCount;
 
-    function addFollower(address child) public {
-        followedBy[msg.sender][child] = true;
-        emit FollowerAdded(msg.sender, child);
+    function follow(address parent) public {
+        following[msg.sender][parent] = true;
+        followedBy[parent][msg.sender] = true;
+        followerCount[parent]++;
+        emit Followed(msg.sender, parent);
     }
 
-    function follow(address parent) public {
-        following[parent][msg.sender] = true;
-        emit Following(msg.sender, parent);
+    function unfollow(address parent) public {
+        following[msg.sender][parent] = false;
+        followedBy[parent][msg.sender] = false;
+        followerCount[parent]--;
+        emit Unfollowed(msg.sender, parent);
     }
 
     function deleteFollower(address child) public {
         followedBy[msg.sender][child] = false;
+        following[child][msg.sender] = false;
+        followerCount[msg.sender]--;
         emit FollowerDeleted(msg.sender, child);
     }
 }
