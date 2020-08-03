cf29603bbd814b543463529304c532e7381e9de9
Nathanael Mueller <nathanaelmueller@Nates-Mac.local>
2020-08-03 14:16:06 -0700

    counter for ids added

--- truffle-project/contracts/Graph.sol
+++ truffle-project/contracts/Graph.sol
@@ -2,32 +2,41 @@
 pragma solidity >=0.4.25 <0.7.0;
 
 contract Graph {
-    event Followed(address child, address parent);
-    event Unfollowed(address child, address parent);
-    event FollowerDeleted(address parent, address child);
+    event Followed(uint id, address child, address parent);
+    event Unfollowed(uint id, address child, address parent);
+    event FollowerDeleted(uint id, address parent, address child);
 
-    mapping(address => mapping(address => bool)) followedBy;
-    mapping(address => mapping(address => bool)) following;
-    mapping(address => uint32) followerCount;
+    mapping(address => mapping(address => bool)) public followedBy;
+    mapping(address => mapping(address => bool)) public following;
+    mapping(address => uint32) public followerCount;
+
+    uint256 private idCounter;
+
+    constructor() public {
+       idCounter = 0;
+    }
 
     function follow(address parent) public {
         following[msg.sender][parent] = true;
         followedBy[parent][msg.sender] = true;
         followerCount[parent]++;
-        emit Followed(msg.sender, parent);
+        idCounter++;
+        emit Followed(idCounter, msg.sender, parent);
     }
 
     function unfollow(address parent) public {
         following[msg.sender][parent] = false;
         followedBy[parent][msg.sender] = false;
         followerCount[parent]--;
-        emit Unfollowed(msg.sender, parent);
+        idCounter++;
+        emit Unfollowed(idCounter, msg.sender, parent);
     }
 
     function deleteFollower(address child) public {
         followedBy[msg.sender][child] = false;
         following[child][msg.sender] = false;
         followerCount[msg.sender]--;
-        emit FollowerDeleted(msg.sender, child);
+        idCounter++;
+        emit FollowerDeleted(idCounter, msg.sender, child);
     }
 }
