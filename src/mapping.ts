c25001bd5bda20437067dfba24d004fdf2a59c3e
Nathanael Mueller <nathanaelmueller@Nates-Mac.local>
2020-08-03 14:17:53 -0700

    mappings for all 3 events

--- src/mapping.ts
+++ src/mapping.ts
@@ -5,50 +5,28 @@ import {
   FollowerDeleted,
   Unfollowed
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Follow, Unfollow, DeleteFollow } from "../generated/schema"
 
 export function handleFollowed(event: Followed): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.child = event.params.child
-  entity.parent = event.params.parent
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // None
+  let id = event.params.id.toHex();
+  let follow = new Follow(id);
+  follow.parent = event.params.parent;
+  follow.child = event.params.child;
+  follow.save()
 }
 
-export function handleFollowerDeleted(event: FollowerDeleted): void {}
+export function handleFollowerDeleted(event: FollowerDeleted): void {
+  let id = event.params.id.toHex();
+  let deleteFollow = new DeleteFollow(id);
+  deleteFollow.parent = event.params.parent;
+  deleteFollow.child = event.params.child;
+  deleteFollow.save()
+}
 
-export function handleUnfollowed(event: Unfollowed): void {}
+export function handleUnfollowed(event: Unfollowed): void {
+  let id = event.params.id.toHex();
+  let unfollow = new Unfollow(id);
+  unfollow.parent = event.params.parent;
+  unfollow.child = event.params.child;
+  unfollow.save()
+}
