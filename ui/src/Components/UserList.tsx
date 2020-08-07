f52110d230cdf06578f749bcaddfc11960818bcc
Nathanael Mueller <nathanaelmueller@Nates-Mac.local>
2020-08-06 22:05:59 -0700

    ui mostly reduces graph data correctly, some links and a logo added

--- ui/src/Components/UserList.tsx
+++ ui/src/Components/UserList.tsx
@@ -6,28 +6,34 @@ import { useQuery, gql } from '@apollo/client';
 
 export const FOLLOWERS_QUERY = gql`
   query GetFollows($profileAddress: Bytes!) {  
-    follows(where: {parent: $profileAddress}) {
+    follows(where: {parent: $profileAddress}, orderBy: id, orderDirection: desc) {
       child
+      id
+    },
+    unfollows(where: {parent: $profileAddress}, orderBy: id, orderDirection: desc) {
+      child
+      id
     }
   }
 `;
 
 const UserList: React.FC<any> = ({account, match, grid}) => {
   const profileAddress = match.params.address
-  const [followed, setFollowed] = useState(false)
   const [loadingButton, setLoadingButton] = useState(false) 
+  // const [followed, setFollowed] = useState(false)
   const { loading, error, data, refetch } = useQuery(FOLLOWERS_QUERY, {variables: { profileAddress }})
 
   useEffect(() => {
     refetch()
-  }, [match, followed])
+  }, [match.params.address])
 
   const handleFollow = () => {
     setLoadingButton(true)
     grid.methods.follow(match.params.address).send({ from: account, gas: 300000 })
     .once('receipt', (receipt: any) => {
       setLoadingButton(false)
-      setFollowed(true)
+      refetch()
+      // setFollowed(true)
     })
   }
 
@@ -36,9 +42,27 @@ const UserList: React.FC<any> = ({account, match, grid}) => {
     grid.methods.unfollow(match.params.address).send({ from: account, gas: 300000 })
     .once('receipt', (receipt: any) => {
       setLoadingButton(false)
-      setFollowed(false)
+      refetch()
+      // setFollowed(false)
     })
   };
+
+  const individualAddresses = () => {
+    let addressSet = new Set<string>()
+    data.follows.forEach((follower: {child: string, id: string}) => {
+      if (!addressSet.has(follower.child)) {
+        addressSet.add(follower.child)
+        for(let i in data.unfollows) {
+          if (parseInt(data.unfollows[i].id, 16) < parseInt(follower.id, 16)) break
+          if (data.unfollows[i].child === follower.child) {
+            addressSet.delete(follower.child)
+            break
+          }
+        }
+      }
+    })
+    return addressSet
+  }
 
   return (
     <div>
@@ -52,29 +76,38 @@ const UserList: React.FC<any> = ({account, match, grid}) => {
         height="150"
       ></img>
       <div className="Username">{match.params.address}</div>
-      {match.params.address === account? null :
-      loadingButton? <button className="Follow Follow-white Follow-animated" disabled>Loading...</button> :
-      !followed ? (
-        <button
-          className="Follow Follow-white Follow-animated"
-          onClick={handleFollow}
-        >
-          Follow
-        </button>
-      ) : (
-        <button
-          className="Follow Follow-white Follow-animated"
-          onClick={() => handleUnfollow()}
-        >
-          Unfollow
-        </button>
-      )}{" "}
-      <div className="Container">
-        {loading ? null :
-        data.follows.map((follower: {child: string}, index: number) => (
-          <List key={index} follower={follower.child} />
-        ))}
-      </div>
+      {loading? null :
+        <div>
+
+          {match.params.address === account? null :
+          (loadingButton)? <button className="Follow Follow-white Follow-animated" disabled>Loading...</button> :
+          !individualAddresses().has(account.toLowerCase()) ? (
+            <button
+              className="Follow Follow-white Follow-animated"
+              onClick={handleFollow}
+            >
+              Follow
+            </button>
+          ) : (
+            <button
+              className="Follow Follow-white Follow-animated"
+              onClick={() => handleUnfollow()}
+            >
+              Unfollow
+            </button>
+          )}
+
+          <div className="Container">
+            {Array.from(individualAddresses()).map((follower: string, index: number) => (
+              <List key={index} follower={follower} />
+            ))}
+          </div>
+
+        </div>
+      }
+
+      
+
     </div>
   )
 }
